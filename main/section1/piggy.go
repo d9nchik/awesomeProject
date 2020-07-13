58818be80f3d385f4f158824bf4b4cb0d6848971
d9nich <dan.galaiko@gmail.com>
2020-07-13 20:42:19 +0300

    A new piggy bank program that uses integers to track the number of cents rather than dollars.

--- main/section1/piggy.go
+++ main/section1/piggy.go
@@ -6,22 +6,22 @@ import (
 )
 
 func main() {
-	account := 0.0
-	for account < 20 {
+	account := 0
+	for account/100 < 20 {
 		switch rand.Intn(3) {
 		case 0:
-			account += 0.05
+			account += 5
 			fmt.Println("Adding nickel: 0.05")
 		case 1:
-			account += 0.1
+			account += 1
 			fmt.Println("Adding dime: 0.1")
 		case 2:
-			account += 0.25
+			account += 25
 			fmt.Println("Adding quarters")
 		}
-		fmt.Printf("Balance: $%.2f\n", account)
+		fmt.Println("Balance: $", float32(account)/100)
 	}
 
 	fmt.Println("Now you can buy a gift!")
-	fmt.Printf("Your balance is $%.2f\n", account)
+	fmt.Println("Your balance is $", float32(account)/100.0)
 }
