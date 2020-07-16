59acf141104094310fd5988682e582801b84a7ea
d9nich <dan.galaiko@gmail.com>
2020-07-16 19:57:12 +0300

    A program that declares a location for each location in list. Each location has been printed.

--- main/section1/landing.go
+++ main/section1/landing.go
@@ -1,28 +1,45 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
+import "fmt"
+
+type location struct {
+	name      string
+	lat, long float64
+}
+
+type coordinate struct {
+	d, m, s float64
+	h       rune
+}
 
 func main() {
-	type location struct {
-		Name string  `json:"name"`
-		Lat  float64 `json:"latitude"`
-		Long float64 `json:"longitude" xml:"longitude"`
+	landingSiteOnMars := []location{
+		newLocation(coordinate{d: 14, m: 34, s: 6.2, h: 'S'}, coordinate{175, 28, 21.5, 'E'},
+			"Columbia Memorial Station"),
+
+		newLocation(coordinate{1, 56, 46.3, 'S'}, coordinate{354, 28, 24.2, 'E'},
+			"Challendger Memorial Station"),
+
+		newLocation(coordinate{4, 35, 22.2, 'S'}, coordinate{137, 26, 30.1, 'E'},
+			"Bradbury Landing"),
+		newLocation(coordinate{4, 30, 0, 'N'}, coordinate{135, 54, 0, 'E'},
+			"Elysium Planitia"),
 	}
 
-	locations := []location{
-		{Name: "Bradbury Landing", Lat: -4.5895, Long: 137.4417},
-		{Name: "Columbia Memorial Station", Lat: -14.5684, Long: 175.472636},
-		{Name: "Challenger Memorial Station", Lat: -1.9462, Long: 354.4734},
+	for _, mar := range landingSiteOnMars {
+		fmt.Println(mar)
 	}
+}
 
-	bytes, err := json.MarshalIndent(locations, " ", " ")
-	if err != nil {
-		fmt.Println("Something gone wrong")
-	} else {
-		fmt.Println(string(bytes))
+func (c coordinate) decimal() float64 {
+	sign := 1.0
+	switch c.h {
+	case 'S', 'W', 's', 'w':
+		sign = -1
 	}
+	return sign * (c.d + c.m/60 + c.s/3600)
+}
 
+func newLocation(lat, long coordinate, name string) location {
+	return location{name: name, lat: lat.decimal(), long: long.decimal()}
 }
